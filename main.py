f40deb3052b7f8348b06b25379b051e980b8e46c
DinaLazenga <d0548559208@gmail.com>
2022-12-18 16:33:54 +0200

    commit gdal

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# import shapefile
+# sf = shapefile.Reader("D:/נווה מדבר/מסמכים/משימות בפייתון/תרגיל 1/AFG_adm2.shp")
+# print(sf)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+# shape = shapefile.Reader("my_shapefile.shp")
+# print(shape)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
